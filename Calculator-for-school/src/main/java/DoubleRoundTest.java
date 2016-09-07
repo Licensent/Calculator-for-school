b8f74e3eb8e8d969f279ba7f7673c0e6a3739203
Licensent <DarthVader.1@mail.ru>
2016-09-07 15:26:36 +0300

    Calc in calc, new Folder

---
+++ Calculator-for-school/src/main/java/DoubleRoundTest.java
@@ -0,0 +1,16 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ * Created by DarthVader on 07.09.2016.
+ */
+public class DoubleRoundTest {
+    double templateDouble =0;
+    public Double doubleRound(Double i){
+        templateDouble =i;
+        double newDouble = new BigDecimal(templateDouble).setScale(4, RoundingMode.UP).doubleValue();
+        return newDouble;
+    }
+
+
+}
